c2cadf3fe9f97b7efffe3bc0ad69001a71e5d9c3
Ratnesh <ratnesh.yadav@staff.ideafloats.com>
2026-02-20 18:01:28 +0530

    Fix SMTP config

--- server.js
+++ server.js
@@ -39,10 +39,15 @@ app.post("/send", async (req, res) => {
   }
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // must be false for 587
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
